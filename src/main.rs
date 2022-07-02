f36fc01856263663ec9230b808a96e792b29fd46
sksat <sksat@sksat.net>
2022-07-02 09:12:06 +0900

    fix: compiler battle

--- src/main.rs
+++ src/main.rs
@@ -129,7 +129,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //let data = light.read(&cmd_char).await?;
     //println!("{:?}", data);
     light.subscribe(tlm_char).await?;
-    let mut nstream = light.notifications().await?;
+    let _nstream = light.notifications().await?;
+
+    let btw_nstream: Vec<_> = futures::stream::iter(btwattch.clone())
+        .then(|bw| async move { bw.notifications().await.unwrap() })
+        //.map(|bw| (true, false))
+        .collect()
+        .await;
+
+    //let mut btw_nstream = Vec::new();
+    //let btw_nstream: Vec<_> = btwattch
+    //    .iter()
+    //    .map(|bw| {
+    //        let rt = tokio::runtime::Runtime::new().unwrap();
+    //        rt.block_on(async {
+    //            let n = bw.notifications().await.unwrap();
+    //        });
+    //    })
+    //    //.map(|bw| (true, false))
+    //    .collect();
 
     let cmd = cmd_char.clone();
     let l2 = light.clone();
@@ -144,50 +162,67 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .await
                 .unwrap();
 
+            for bw in btwattch.iter() {
+                bw.write(&cmd, &payload, WriteType::WithoutResponse)
+                    .await
+                    .unwrap();
+            }
+
             time::sleep(Duration::from_millis(1000)).await;
         }
     });
 
-    let mut data_buf = Vec::new();
+    //let mut btw_nstream: Vec<(_, Vec<u8>)> = btw_nstream
+    //    .iter()
+    //    .map(move |mut ns| (ns, Vec::new()))
+    //    .collect();
+    let len = btw_nstream.len();
+    let btw_nstream: Vec<(_, Vec<u8>)> =
+        btw_nstream.into_iter().zip(vec![Vec::new(); len]).collect();
 
-    while let Some(data) = nstream.next().await {
-        // receive to buf
-        if data.value[0] == 0xAA {
-            data_buf = data.value;
-        } else {
-            data_buf.extend(data.value);
+    for mut nstream in btw_nstream {
+        let data_buf = &mut nstream.1;
+        let nstream = &mut nstream.0;
+        while let Some(data) = nstream.next().await {
+            // receive to buf
+            if data.value[0] == 0xAA {
+                *data_buf = data.value;
+            } else {
+                data_buf.extend(data.value);
+            }
+
+            println!("recv: {:x?}", data_buf);
+
+            if data_buf.len() < 23 {
+                continue;
+            }
+
+            // deserialize
+
+            let mut voltage = vec![0; 6];
+            voltage.copy_from_slice(&data_buf[5..11]);
+            voltage.extend_from_slice(&[0, 0]);
+            let voltage: [u8; 8] = voltage.try_into().unwrap();
+            let voltage = i64::from_le_bytes(voltage);
+            let voltage = voltage as f32 / 16777216.0;
+
+            let mut current = vec![0; 6];
+            current.copy_from_slice(&data_buf[11..17]);
+            current.extend_from_slice(&[0, 0]);
+            let current: [u8; 8] = current.try_into().unwrap();
+            let current = i64::from_le_bytes(current);
+            let current = current as f32 / 1073741824.0;
+
+            let mut wattage = vec![0; 6];
+            wattage.copy_from_slice(&data_buf[17..23]);
+            wattage.extend_from_slice(&[0, 0]);
+            let wattage: [u8; 8] = wattage.try_into().unwrap();
+            let wattage = i64::from_le_bytes(wattage);
+            let wattage = wattage as f32 / 16777216.0;
+
+            println!("V = {}, A = {}, W = {}", voltage, current, wattage);
         }
-
-        println!("recv: {:x?}", data_buf);
-
-        if data_buf.len() < 23 {
-            continue;
-        }
-
-        // deserialize
-
-        let mut voltage = vec![0; 6];
-        voltage.copy_from_slice(&data_buf[5..11]);
-        voltage.extend_from_slice(&[0, 0]);
-        let voltage: [u8; 8] = voltage.try_into().unwrap();
-        let voltage = i64::from_le_bytes(voltage);
-        let voltage = voltage as f32 / 16777216.0;
-
-        let mut current = vec![0; 6];
-        current.copy_from_slice(&data_buf[11..17]);
-        current.extend_from_slice(&[0, 0]);
-        let current: [u8; 8] = current.try_into().unwrap();
-        let current = i64::from_le_bytes(current);
-        let current = current as f32 / 1073741824.0;
-
-        let mut wattage = vec![0; 6];
-        wattage.copy_from_slice(&data_buf[17..23]);
-        wattage.extend_from_slice(&[0, 0]);
-        let wattage: [u8; 8] = wattage.try_into().unwrap();
-        let wattage = i64::from_le_bytes(wattage);
-        let wattage = wattage as f32 / 16777216.0;
-
-        println!("V = {}, A = {}, W = {}", voltage, current, wattage);
     }
+
     Ok(())
 }
